a232325232e99b7ad3969313c65c45d9ff3c9a22
UnkwUsr <ktoto2707043@gmail.com>
2023-10-19 13:55:19 +0300

    refactor: move processing messages from msg_queue to vk

--- vk.py
+++ vk.py
@@ -1,6 +1,9 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType, VkLongpollMode
 
+import json
+from pprint import pprint
+
 
 class Vk:
     session = None
@@ -24,6 +27,7 @@ class Vk:
                         event._load_attachments = lambda: self.vk.messages.getById(
                             message_ids=event.message_id
                         )
+                        event._process_message = self.process_message
 
                         callback(event)
                 break
@@ -31,3 +35,59 @@ class Vk:
                 print("vk listen() exception:")
                 print(e)
                 continue
+
+    def process_message(self, event):
+        author_id = None
+        if hasattr(event, "user_id"):
+            author_id = event.user_id
+        if hasattr(event, "group_id"):
+            author_id = event.group_id
+
+        text = event._get_id_name(author_id) + ": " + event.message
+
+        if event.attachments:
+            attachments = event._load_attachments()["items"]
+            for item in attachments:
+                print("Attachment: ", end="")
+                pprint(item)
+                if "reply_message" in item.keys():
+                    # TODO: probably also take attachments from there,
+                    # recursive
+                    reply = item["reply_message"]
+                    text += (
+                        "\n---\nReplied to: "
+                        + event._get_id_name(abs(reply["from_id"]))
+                        + ": "
+                        + reply["text"]
+                    )
+                if "fwd_messages" in item.keys():
+                    # TODO: probably also take attachments from there,
+                    # recursive
+                    fwds = item["fwd_messages"]
+                    for fwd in fwds:
+                        text += (
+                            "\n---\nForwarded: "
+                            + event._get_id_name(abs(fwd["from_id"]))
+                            + ": "
+                            + fwd["text"]
+                        )
+                        # stub, we do not support deep forwarded messages. On
+                        # the other hand, vk does the same on the site. But api
+                        # in real returns all forwards chain
+                        if "fwd_messages" in fwd.keys():
+                            text += "*reforwarded messages*"
+
+                # yes, yet another attribute with name "attachments"
+                for itach in item["attachments"]:
+                    if "photo" in itach.keys():
+                        # TOOD: probably should take proper size. Some of them
+                        # can be cropped, this is bad
+                        # https://dev.vk.com/ru/reference/objects/photo-sizes
+                        text += "\nPhoto: " + itach["photo"]["sizes"][-1]["url"]
+                    if "audio_message" in itach.keys():
+                        # TODO: ogg or mp3?
+                        text += "\nVoice: " + itach["audio_message"]["link_ogg"]
+
+            text += "\n---\nRaw attachments: \n" + json.dumps(event.attachments)
+
+        return text
