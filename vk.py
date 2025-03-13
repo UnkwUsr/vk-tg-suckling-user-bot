36859ed80129fb51dd6cfbfc9cc311dc36e2000e
UnkwUsr <ktoto2707043@gmail.com>
2025-03-13 23:15:24 +0300

    feat: support wall_reply

--- vk.py
+++ vk.py
@@ -85,6 +85,11 @@ class Vk:
                 wall = attach["wall"]
                 url = "vk.com/wall{0}_{1}".format(wall["owner_id"], wall["id"])
                 text += "\nWall: {0}\n{1}".format(url, wall["text"])
+            if "wall_reply" in attach.keys():
+                wall_reply = attach["wall_reply"]
+                url = "vk.com/wall{0}_{1}?reply={2}".format(wall_reply["owner_id"], wall_reply["post_id"], wall_reply["id"])
+                text += "\nWall reply: {0}\n".format(url)
+                text += self.recursive_process_message(wall_reply)
 
         if "reply_message" in message.keys():
             reply = message["reply_message"]
