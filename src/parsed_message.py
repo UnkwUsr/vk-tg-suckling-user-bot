ac0f92b161b683afa6435ef95a027a5bc67144b2
UnkwUsr <ktoto2707043@gmail.com>
2025-04-06 18:46:16 +0300

    fix(ParsedMessage): make fields of object instead of fields of class
    this caused fields initialize only once on python start, and then they
    was used for all objects of this class

--- src/parsed_message.py
+++ src/parsed_message.py
@@ -1,13 +1,15 @@
 # this is resulting struct of message, parsed from vk and passed then to
 # telegram bridge
 class ParsedMessage:
-    text = ""
-    previews = []
-    video_downloaded_file = None
+    def __init__(self):
+        self.text = ""
+        self.previews = []
+        self.video_downloaded_file = None
 
     # operator +=
     def __iadd__(self, other):
         self.text += other.text
+        self.previews.extend(other.previews)
         # TODO: now this only honors latest attached video. Possible cases:
         # multiple videos attached; message contained video which is itself a
         # reply by another message contained video (this case can be ignored, I
